84729d3e119a3f15013f13efcdbec56b22166ff2
Raniery Alves Vasconcelos <58216614+RanieryAV@users.noreply.github.com>
2023-11-19 13:13:06 -0300

    Versão preliminar da função que gera a letra "P"

--- js/main.js
+++ js/main.js
@@ -1,73 +1,69 @@
-var mousePos = {x:.5,y:.5};
-document.addEventListener('mousemove', function (event) {  mousePos = {x:event.clientX/window.innerWidth, y:event.clientY/window.innerHeight};});
-var phase = 0;
-
 var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(95, window.innerWidth / window.innerHeight, 0.1, 1000);
-camera.position.z = 30;
-
+var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var boxSize = 0.2;
-var geometry = new THREE.BoxGeometry(boxSize, boxSize, boxSize);
-var materialGreen = new THREE.MeshBasicMaterial({transparent: true,  color: 0xff0000,  opacity: 0.4,  side: THREE.DoubleSide});
+var verticalLineParticleGeometry = new THREE.BufferGeometry();
+var particleMaterial = new THREE.PointsMaterial({ color: 0xffffff, size: 0.001 });
 
-var pitchSegments = 60;
-var elevationSegments = pitchSegments/2;
-var particles = pitchSegments*elevationSegments
-var side = Math.pow(particles, 1/3);
+var rightHalfOfCircleParticleGeometry = new THREE.BufferGeometry();
 
-var radius = 16;
+var positions = new Float32Array(1000 * 3); // An array to store particle positions
+var circle_positions = new Float32Array(1000 * 3); // An array to store particle positions
 
-var parentContainer = new THREE.Object3D();
-scene.add(parentContainer);
+// Function to generate particles forming the letter "P"
+function generateLetterP() {
+  for (var i = 0; i < 500; i++) {
+    // Distribute particles in a semicircular shape
+    var angle = (i / 500) * Math.PI;
 
-function posInBox(place) {
-  return ((place/side) - 0.5) * radius * 1.2;  
+    // Position particles to form the letter "P"
+    circle_positions[i / 5 * 3] = Math.sin(angle);
+    circle_positions[i / 5 * 3 + 1] = Math.cos(angle);
+    
+    // Distribute particles along a horizontal line with fewer particles
+    if (i % 5 === 0) {
+      positions[i / 9 * 3] = (i / 1000000) * 2;
+      positions[i / 9 * 3 + 1] = 0;
+      positions[i / 9 * 3 + 2] = 0;
+    }
+
+    // Add a vertical line on the left side of the horizontal line
+    if (i < 375) {
+      positions[i / 5 * 3 + 1] = (i / 250) * 2 - 2; // Adjust the length of the vertical line
+    }
+  }
+
+  verticalLineParticleGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+  var verticalLineParticleSystem = new THREE.Points(verticalLineParticleGeometry, particleMaterial);
+  scene.add(verticalLineParticleSystem);
+
+  rightHalfOfCircleParticleGeometry.setAttribute('position', new THREE.BufferAttribute(circle_positions, 3));
+  var rightHalfOfCircleParticleSystem = new THREE.Points(rightHalfOfCircleParticleGeometry, particleMaterial);
+  scene.add(rightHalfOfCircleParticleSystem);
 }
 
-//Plant the seeds, grow some trees in a grid!
-for (var p = 0; p < pitchSegments; p++) {
-  var pitch = Math.PI * 2 * p / pitchSegments ;
-  for (var e = 0; e < elevationSegments; e++) {
-    var elevation = Math.PI  * ((e / elevationSegments)-0.5)
-    var particle = new THREE.Mesh(geometry, materialGreen);
-    
-    
-    parentContainer.add(particle);
+generateLetterP();
 
-    var dest = new THREE.Vector3();
-    dest.z = (Math.sin(pitch) * Math.cos(elevation)) * radius; //z pos in sphere
-    dest.x = (Math.cos(pitch) * Math.cos(elevation)) * radius; //x pos in sphere
-    dest.y = Math.sin(elevation) * radius; //y pos in sphere
+camera.position.z = 3;
 
-    particle.position.x = posInBox(parentContainer.children.length % side);
-    particle.position.y = posInBox(Math.floor(parentContainer.children.length/side) % side);
-    particle.position.z = posInBox(Math.floor(parentContainer.children.length/Math.pow(side,2)) % side);
-    console.log(side, parentContainer.children.length, particle.position.x, particle.position.y, particle.position.z)
-    particle.userData = {dests: [dest,particle.position.clone()], speed: new THREE.Vector3() };
-  }
-}
+function animate() {
+  requestAnimationFrame(animate);
 
-function render() {
-  phase += 0.002;
-  for (var i = 0, l = parentContainer.children.length; i < l; i++) {
-    var particle = parentContainer.children[i];
-    var dest = particle.userData.dests[Math.floor(phase)%particle.userData.dests.length].clone();
-    var diff = dest.sub(particle.position);
-    particle.userData.speed.divideScalar(1.02); // Some drag on the speed
-    particle.userData.speed.add(diff.divideScalar(400));// Modify speed by a fraction of the distance to the dest    
-    particle.position.add(particle.userData.speed);
-    particle.lookAt(dest);
-  }
-  
-  parentContainer.rotation.y = phase*3;
-  parentContainer.rotation.x = (mousePos.y-0.5) * Math.PI;
-  parentContainer.rotation.z = (mousePos.x-0.5) * Math.PI;
+  // Update particle positions or any other animations here
 
   renderer.render(scene, camera);
-  requestAnimationFrame(render);
 }
-render();
+
+animate();
+
+window.addEventListener('resize', function () {
+  var newWidth = window.innerWidth;
+  var newHeight = window.innerHeight;
+
+  camera.aspect = newWidth / newHeight;
+  camera.updateProjectionMatrix();
+
+  renderer.setSize(newWidth, newHeight);
+});
