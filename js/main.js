dc951327efc25a5ca121231c4bd4f16d27700d1d
Raniery Alves Vasconcelos <58216614+RanieryAV@users.noreply.github.com>
2023-12-01 16:18:23 -0300

    Nuvem giratória de partículas e texto 3D (à parte)

--- js/main.js
+++ js/main.js
@@ -1,69 +1,150 @@
-var scene = new THREE.Scene();
-var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
-var renderer = new THREE.WebGLRenderer();
+import * as THREE from "https://cdn.skypack.dev/three@0.133.1/build/three.module";
+import { OrbitControls } from 'https://cdn.skypack.dev/three@0.133.1/examples/jsm/controls/OrbitControls';
+import { FontLoader } from "https://cdn.skypack.dev/three@0.133.1/examples/jsm/loaders/FontLoader.js";
+import { TextGeometry } from "https://cdn.skypack.dev/three@0.133.1/examples/jsm/geometries/TextGeometry.js";
+import { TweenMax, Power4, Power2 } from "gsap";
+
+// Constants
+const particleCount = 6000;
+const particleSize = 0.3;
+const defaultAnimationSpeed = 1;
+const morphAnimationSpeed = 18;
+const color = '#FFFFFF';
+
+// Triggers
+const triggers = document.getElementsByTagName('span');
+
+// Animation variables
+let animationVars = {
+    speed: defaultAnimationSpeed / 100,
+    color: color,
+    rotation: -45
+};
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
+camera.position.set(0, 0, 300);
+
+const renderer = new THREE.WebGLRenderer();
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-var verticalLineParticleGeometry = new THREE.BufferGeometry();
-var particleMaterial = new THREE.PointsMaterial({ color: 0xffffff, size: 0.001 });
+const controls = new OrbitControls(camera, renderer.domElement);
+controls.update();
 
-var rightHalfOfCircleParticleGeometry = new THREE.BufferGeometry();
+// Function to create random points in a buffer geometry
+function generateRandomPointsInBufferGeometry(geometry, count) {
+    const positions = geometry.attributes.position.array;
+    const result = new Float32Array(count * 3);
 
-var positions = new Float32Array(1000 * 3); // An array to store particle positions
-var circle_positions = new Float32Array(1000 * 3); // An array to store particle positions
+    for (let i = 0; i < count; i++) {
+        const index = Math.floor(Math.random() * (positions.length / 3));
+        const offset = index * 3;
 
-// Function to generate particles forming the letter "P"
-function generateLetterP() {
-  for (var i = 0; i < 500; i++) {
-    // Distribute particles in a semicircular shape
-    var angle = (i / 500) * Math.PI;
-
-    // Position particles to form the letter "P"
-    circle_positions[i / 5 * 3] = Math.sin(angle);
-    circle_positions[i / 5 * 3 + 1] = Math.cos(angle);
-    
-    // Distribute particles along a horizontal line with fewer particles
-    if (i % 5 === 0) {
-      positions[i / 9 * 3] = (i / 1000000) * 2;
-      positions[i / 9 * 3 + 1] = 0;
-      positions[i / 9 * 3 + 2] = 0;
+        result[i * 3] = positions[offset];
+        result[i * 3 + 1] = positions[offset + 1];
+        result[i * 3 + 2] = positions[offset + 2];
     }
 
-    // Add a vertical line on the left side of the horizontal line
-    if (i < 375) {
-      positions[i / 5 * 3 + 1] = (i / 250) * 2 - 2; // Adjust the length of the vertical line
+    const bufferGeometry = new THREE.BufferGeometry();
+    bufferGeometry.setAttribute('position', new THREE.BufferAttribute(result, 3));
+
+    return bufferGeometry;
+}
+
+// Function to create vertices for points
+function createVertices(bufferGeometry, points) {
+    const positionAttribute = bufferGeometry.getAttribute('position');
+
+    if (!positionAttribute) {
+        console.error("Position attribute is undefined. BufferGeometry might not be correctly initialized.");
+        return;
     }
-  }
 
-  verticalLineParticleGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
-  var verticalLineParticleSystem = new THREE.Points(verticalLineParticleGeometry, particleMaterial);
-  scene.add(verticalLineParticleSystem);
+    const geometry = new THREE.Geometry();
 
-  rightHalfOfCircleParticleGeometry.setAttribute('position', new THREE.BufferAttribute(circle_positions, 3));
-  var rightHalfOfCircleParticleSystem = new THREE.Points(rightHalfOfCircleParticleGeometry, particleMaterial);
-  scene.add(rightHalfOfCircleParticleSystem);
+    for (let p = 0; p < particleCount; p++) {
+        const vertex = new THREE.Vector3();
+        vertex.fromBufferAttribute(positionAttribute, p);
+
+        geometry.vertices.push(vertex);
+    }
+
+    // Set the geometry directly for points
+    points.geometry = geometry;
 }
 
-generateLetterP();
+// Create particles
+const particlesGeometry = new THREE.BufferGeometry();
+const particlesMaterial = new THREE.PointsMaterial({ size: particleSize, vertexColors: true });
 
-camera.position.z = 3;
+// Generate random particles
+const positions = [];
+const colors = [];
 
-function animate() {
-  requestAnimationFrame(animate);
+for (let i = 0; i < particleCount; i++) {
+    const x = (Math.random() - 0.5) * 200;
+    const y = (Math.random() - 0.5) * 200;
+    const z = (Math.random() - 0.5) * 200;
 
-  // Update particle positions or any other animations here
+    positions.push(x, y, z);
 
-  renderer.render(scene, camera);
+    const color = new THREE.Color();
+    color.setHSL(Math.random(), 1.0, 0.5);
+
+    colors.push(color.r, color.g, color.b);
 }
 
-animate();
+particlesGeometry.setAttribute('position', new THREE.Float32BufferAttribute(positions, 3));
+particlesGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
 
-window.addEventListener('resize', function () {
-  var newWidth = window.innerWidth;
-  var newHeight = window.innerHeight;
+const particleSystem = new THREE.Points(particlesGeometry, particlesMaterial);
+scene.add(particleSystem);
 
-  camera.aspect = newWidth / newHeight;
-  camera.updateProjectionMatrix();
+// Texts
+var loader = new FontLoader();
+var typeface = 'https://dl.dropboxusercontent.com/s/bkqic142ik0zjed/swiss_black_cond.json?';
 
-  renderer.setSize(newWidth, newHeight);
+loader.load(typeface, (font) => {
+    Array.from(triggers).forEach((trigger, idx) => {
+        const textGeometry = new TextGeometry(trigger.textContent, {
+            font: font,
+            size: window.innerWidth * 0.02,
+            height: 4,
+            curveSegments: 10,
+        });
+        textGeometry.center();
+
+        const textMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
+        const textMesh = new THREE.Mesh(textGeometry, textMaterial);
+
+        // Position the text
+        textMesh.position.x = (idx - 1) * 100; // Adjust the x position based on the index
+        textMesh.position.y = 50;
+
+        scene.add(textMesh);
+    });
+
+    // Animate
+    // Animate
+    function animate() {
+      particleSystem.rotation.y += animationVars.speed;
+      particleSystem.geometry.verticesNeedUpdate = true;
+
+      // Update controls
+      controls.update();
+
+      // Update camera
+      camera.lookAt(scene.position);
+
+      particleSystem.material.color = new THREE.Color(animationVars.color);
+
+      window.requestAnimationFrame(animate);
+      renderer.render(scene, camera);
+    }
+
+
+
+    animate();
 });
