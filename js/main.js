779fc183e9c8d3e9d24cbf7150ee21c71ff77b5a
Raniery Alves Vasconcelos <58216614+RanieryAV@users.noreply.github.com>
2023-12-02 01:33:02 -0300

    Texto 3D composto  de partículas

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ import { TextGeometry } from "https://cdn.skypack.dev/three@0.133.1/examples/jsm
 import { TweenMax, Power4, Power2 } from "gsap";
 
 // Constants
-const particleCount = 6000;
-const particleSize = 0.3;
+const particleCount = 100000;
+const particleSize = 0.9;
 const defaultAnimationSpeed = 1;
 const morphAnimationSpeed = 18;
 const color = '#FFFFFF';
@@ -23,7 +23,7 @@ let animationVars = {
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
-camera.position.set(0, 0, 300);
+camera.position.set(85, 290, 130);
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -75,6 +75,72 @@ function createVertices(bufferGeometry, points) {
     points.geometry = geometry;
 }
 
+function disperseParticlesDEPRECATED(textGeometry, count) {
+    const particlesGeometry = new THREE.BufferGeometry();
+
+    const positions = textGeometry.attributes.position.array;
+    const boundingBox = new THREE.Box3().setFromBufferAttribute(textGeometry.attributes.position);
+
+    const particleCount = count;
+    const particlePositions = new Float32Array(particleCount * 3);
+    const particleColors = new Float32Array(particleCount * 3);
+
+    for (let i = 0; i < particleCount; i++) {
+        const randomPoint = new THREE.Vector3(
+            Math.random() * boundingBox.getSize(new THREE.Vector3()).x + boundingBox.min.x,
+            Math.random() * boundingBox.getSize(new THREE.Vector3()).y + boundingBox.min.y,
+            Math.random() * boundingBox.getSize(new THREE.Vector3()).z + boundingBox.min.z
+        );
+
+        particlePositions[i * 3] = randomPoint.x;
+        particlePositions[i * 3 + 1] = randomPoint.y;
+        particlePositions[i * 3 + 2] = randomPoint.z;
+
+        const color = new THREE.Color();
+        color.setHSL(Math.random(), 1.0, 0.5);
+
+        particleColors[i * 3] = color.r;
+        particleColors[i * 3 + 1] = color.g;
+        particleColors[i * 3 + 2] = color.b;
+    }
+
+    particlesGeometry.setAttribute('position', new THREE.BufferAttribute(particlePositions, 3));
+    particlesGeometry.setAttribute('color', new THREE.BufferAttribute(particleColors, 3));
+
+    return particlesGeometry;
+}
+
+function generateSparseParticles(geometry, particleCount) {
+    const positions = geometry.attributes.position.array;
+    const result = new Float32Array(particleCount * 3);
+  
+    for (let i = 0; i < particleCount; i++) {
+      const index = Math.floor(Math.random() * (positions.length / 3));
+      const offset = index * 3;
+  
+      result[i * 3] = positions[offset];
+      result[i * 3 + 1] = positions[offset + 1];
+      result[i * 3 + 2] = positions[offset + 2];
+    }
+  
+    const bufferGeometry = new THREE.BufferGeometry();
+    bufferGeometry.setAttribute('position', new THREE.BufferAttribute(result, 3));
+  
+    return bufferGeometry;
+  }
+  
+  function disperseParticles(geometry, particleCount) {
+    const particlesGeometry = generateSparseParticles(geometry, particleCount);
+    const particlesMaterial = new THREE.PointsMaterial({ size: particleSize, vertexColors: true });
+  
+    const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+    return particles;
+  }
+  
+
+
+
+
 // Create particles
 const particlesGeometry = new THREE.BufferGeometry();
 const particlesMaterial = new THREE.PointsMaterial({ size: particleSize, vertexColors: true });
@@ -100,7 +166,7 @@ particlesGeometry.setAttribute('position', new THREE.Float32BufferAttribute(posi
 particlesGeometry.setAttribute('color', new THREE.Float32BufferAttribute(colors, 3));
 
 const particleSystem = new THREE.Points(particlesGeometry, particlesMaterial);
-scene.add(particleSystem);
+//scene.add(particleSystem);
 
 // Texts
 var loader = new FontLoader();
@@ -108,43 +174,51 @@ var typeface = 'https://dl.dropboxusercontent.com/s/bkqic142ik0zjed/swiss_black_
 
 loader.load(typeface, (font) => {
     Array.from(triggers).forEach((trigger, idx) => {
-        const textGeometry = new TextGeometry(trigger.textContent, {
-            font: font,
-            size: window.innerWidth * 0.02,
-            height: 4,
-            curveSegments: 10,
-        });
-        textGeometry.center();
-
-        const textMaterial = new THREE.MeshBasicMaterial({ color: 0xffffff });
-        const textMesh = new THREE.Mesh(textGeometry, textMaterial);
-
-        // Position the text
-        textMesh.position.x = (idx - 1) * 100; // Adjust the x position based on the index
-        textMesh.position.y = 50;
-
-        scene.add(textMesh);
+      const textGeometry = new TextGeometry(trigger.textContent, {
+        font: font,
+        size: window.innerWidth * 0.02,
+        height: 4,
+        curveSegments: 10,
+      });
+      textGeometry.center();
+  
+      // Create particles for each character
+      const positions = textGeometry.attributes.position.array;
+  
+      for (let i = 0; i < positions.length; i += 3) {
+        const x = positions[i];
+        const y = positions[i + 1];
+        const z = positions[i + 2];
+  
+        const particlesGeometry = new THREE.BufferGeometry();
+        const particlesMaterial = new THREE.PointsMaterial({ size: particleSize, vertexColors: true });
+  
+        const particlePosition = new THREE.Float32BufferAttribute([x, y, z], 3);
+        const particleColor = new THREE.Float32BufferAttribute([1, 1, 1], 3);
+  
+        particlesGeometry.setAttribute('position', particlePosition);
+        particlesGeometry.setAttribute('color', particleColor);
+  
+        const particles = new THREE.Points(particlesGeometry, particlesMaterial);
+  
+        // Position the particles
+        particles.position.x = (idx - 1) * 100;
+        particles.position.y = 50;
+  
+        scene.add(particles);
+      }
     });
-
-    // Animate
     // Animate
     function animate() {
-      particleSystem.rotation.y += animationVars.speed;
-      particleSystem.geometry.verticesNeedUpdate = true;
+        // Update controls
+        controls.update();
 
-      // Update controls
-      controls.update();
+        // Update camera
+        camera.lookAt(scene.position);
 
-      // Update camera
-      camera.lookAt(scene.position);
-
-      particleSystem.material.color = new THREE.Color(animationVars.color);
-
-      window.requestAnimationFrame(animate);
-      renderer.render(scene, camera);
+        window.requestAnimationFrame(animate);
+        renderer.render(scene, camera);
     }
-
-
 
     animate();
 });
