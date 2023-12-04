c94c7143cbbcfe6c29c7fb7bc330c249652930f3
Raniery Alves Vasconcelos <58216614+RanieryAV@users.noreply.github.com>
2023-12-03 23:52:51 -0300

    Duas letras P com base no código do Vinicius Luis

--- js/main.js
+++ js/main.js
@@ -21,15 +21,21 @@ let animationVars = {
     rotation: -45
 };
 
+<<<<<<< Updated upstream
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
 camera.position.set(85, 290, 130);
+=======
+const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 10
+>>>>>>> Stashed changes
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
+<<<<<<< Updated upstream
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.update();
 
@@ -37,6 +43,12 @@ controls.update();
 function generateRandomPointsInBufferGeometry(geometry, count) {
     const positions = geometry.attributes.position.array;
     const result = new Float32Array(count * 3);
+=======
+// ------------------------------------ criação das particulas do primeiro (P) ------------------------------------ //
+
+const quantipart = 1500
+const sizeofpart = 0.5
+>>>>>>> Stashed changes
 
     for (let i = 0; i < count; i++) {
         const index = Math.floor(Math.random() * (positions.length / 3));
@@ -50,7 +62,125 @@ function generateRandomPointsInBufferGeometry(geometry, count) {
     const bufferGeometry = new THREE.BufferGeometry();
     bufferGeometry.setAttribute('position', new THREE.BufferAttribute(result, 3));
 
+<<<<<<< Updated upstream
     return bufferGeometry;
+=======
+const particles4 = new THREE.Geometry()
+randomparticles(quantipart, 200, 200, 200, particles4)
+const particleMaterial4 = new THREE.PointsMaterial
+({
+  color: 0xFFFFFF, 
+  size: sizeofpart 
+});
+const particleSystem4 = new THREE.Points(particles4, particleMaterial4);
+
+// ------------------------------------ Altera posição das particulas para formar o P ------------------------------------ //
+
+particleSystem2.position.y += 20
+particleSystem2.position.x += 10
+particleSystem3.position.y += 10
+particleSystem3.position.x += 20
+particleSystem4.position.x += 10
+
+// ------------------------------------ Adiciona os sistemas a um grupo único ------------------------------------ //
+
+const groupP = new Group
+groupP.add(particleSystem1)
+groupP.add(particleSystem2)
+groupP.add(particleSystem3)
+groupP.add(particleSystem4)
+scene.add(groupP)
+
+// ------------------------------------ criação das particulas do segundo (P) ------------------------------------ //
+const particles5 = new THREE.Geometry() // Cria a coluna vertical do P
+randomparticles(quantipart, 200, 200, 200, particles5)
+const particleMaterial5 = new THREE.PointsMaterial
+({
+  color: 0xFF0000, 
+  size: sizeofpart 
+});
+const particleSystem5 = new THREE.Points(particles5, particleMaterial5);
+
+const particles6 = new THREE.Geometry()
+randomparticles(quantipart, 200, 200, 200, particles6)
+const particleMaterial6 = new THREE.PointsMaterial
+({
+  color: 0x00FF00, 
+  size: sizeofpart 
+});
+const particleSystem6 = new THREE.Points(particles6, particleMaterial6);
+
+const particles7 = new THREE.Geometry()
+randomparticles(quantipart, 200, 200, 200, particles7)
+const particleMaterial7 = new THREE.PointsMaterial
+({
+  color: 0x0000FF, 
+  size: sizeofpart 
+});
+const particleSystem7 = new THREE.Points(particles7, particleMaterial7);
+
+const particles8 = new THREE.Geometry()
+randomparticles(quantipart, 200, 200, 200, particles8)
+const particleMaterial8 = new THREE.PointsMaterial
+({
+  color: 0xFFFFFF, 
+  size: sizeofpart 
+});
+const particleSystem8 = new THREE.Points(particles8, particleMaterial8);
+
+// ------------------------------------ Altera posição das particulas para formar o P ------------------------------------ //
+
+particleSystem5.position.x += 40
+particleSystem6.position.y += 20
+particleSystem6.position.x += 50
+particleSystem7.position.y += 10
+particleSystem7.position.x += 60
+particleSystem8.position.x += 50
+
+// ------------------------------------ Adiciona os sistemas a um grupo único ------------------------------------ //
+
+const groupP2 = new Group
+groupP2.add(particleSystem5)
+groupP2.add(particleSystem6)
+groupP2.add(particleSystem7)
+groupP2.add(particleSystem8)
+scene.add(groupP2)
+// ------------------------------------ controle orbital da camera ------------------------------------ //
+
+const controls =  new OrbitControls(camera, renderer.domElement)
+
+// ------------------------------------ Animação ------------------------------------ //
+
+const velocityofpart = 0.5
+function animate() 
+{
+  requestAnimationFrame(animate);
+
+  // As particulas de cada sistema vão para determinadas posições e formam diferentes formas cubicas
+
+  animateletterformation(particles1, particleSystem1, particles1.particle, 5, 25, 5, velocityofpart)
+  animateletterformation(particles2, particleSystem2, particles2.particle, 15, 5, 5, velocityofpart)
+  animateletterformation(particles3, particleSystem3, particles3.particle, 5, 15, 5, velocityofpart)
+  animateletterformation(particles4, particleSystem4, particles4.particle, 15, 5, 5, velocityofpart)
+
+  animateletterformation(particles5, particleSystem5, particles5.particle, 5, 25, 5, velocityofpart)
+  animateletterformation(particles6, particleSystem6, particles6.particle, 15, 5, 5, velocityofpart)
+  animateletterformation(particles7, particleSystem7, particles7.particle, 5, 15, 5, velocityofpart)
+  animateletterformation(particles8, particleSystem8, particles8.particle, 15, 5, 5, velocityofpart)
+  
+  // translação e rotação do grupo todo
+  
+  //groupP.position.x += Math.cos(Math.PI) + groupP.position.z/100
+  //groupP.position.z += Math.sin(Math.PI) - groupP.position.x/100
+  
+  //groupP.rotation.y += 0.1
+  
+  
+  // atualiza a cena e o controle de camera a cada frame
+
+  controls.update() 
+  renderer.render(scene, camera);
+>>>>>>> Stashed changes
 }
 
 // Function to create vertices for points
